8a3b69115c3e5ce484b8d68cf3973bea6230637c
Taylor Monacelli <taylormonacelli@gmail.com>
2024-01-27 20:03:35 -0800

    remove not-helpful comments

--- bicycle/bicycle.go
+++ bicycle/bicycle.go
@@ -10,7 +10,6 @@ type Bicycle struct {
 	Weight int
 }
 
-// NewBicycle creates a new Bicycle instance with default values and allows specifying properties.
 func NewBicycle(properties ...func(*Bicycle)) *Bicycle {
 	bike := &Bicycle{
 		Brand:  "DefaultBrand",
@@ -26,33 +25,28 @@ func NewBicycle(properties ...func(*Bicycle)) *Bicycle {
 	return bike
 }
 
-// WithBrand is a function to set the brand property of a Bicycle.
 func WithBrand(brand string) func(*Bicycle) {
 	return func(b *Bicycle) {
 		b.Brand = brand
 	}
 }
 
-// WithColor is a function to set the color property of a Bicycle.
 func WithColor(color string) func(*Bicycle) {
 	return func(b *Bicycle) {
 		b.Color = color
 	}
 }
 
-// WithWeight is a function to set the weight property of a Bicycle.
 func WithWeight(weight int) func(*Bicycle) {
 	return func(b *Bicycle) {
 		b.Weight = weight
 	}
 }
 
-// Pedal peddles the bicycle.
 func (b *Bicycle) Pedal() {
 	fmt.Println("Pedaling the bicycle.")
 }
 
-// Brake applies the brakes to the bicycle.
 func (b *Bicycle) Brake() {
 	fmt.Println("Applying brakes to the bicycle.")
 }
--- sailboat/sailboat.go
+++ sailboat/sailboat.go
@@ -1,4 +1,3 @@
-// sailboat/builder.go
 package sailboat
 
 type Sailboat struct {
@@ -7,12 +6,10 @@ type Sailboat struct {
 	Length int
 }
 
-// SailboatBuilder is a builder for creating Sailboat instances.
 type SailboatBuilder struct {
 	boat *Sailboat
 }
 
-// NewSailboatBuilder creates a new SailboatBuilder with default values.
 func NewSailboatBuilder() *SailboatBuilder {
 	return &SailboatBuilder{boat: &Sailboat{
 		Brand:  "Unknown",
@@ -21,25 +18,21 @@ func NewSailboatBuilder() *SailboatBuilder {
 	}}
 }
 
-// Brand sets the brand property of the sailboat.
 func (sb *SailboatBuilder) Brand(brand string) *SailboatBuilder {
 	sb.boat.Brand = brand
 	return sb
 }
 
-// Color sets the color property of the sailboat.
 func (sb *SailboatBuilder) Color(color string) *SailboatBuilder {
 	sb.boat.Color = color
 	return sb
 }
 
-// Length sets the length property of the sailboat.
 func (sb *SailboatBuilder) Length(length int) *SailboatBuilder {
 	sb.boat.Length = length
 	return sb
 }
 
-// Build creates the final Sailboat instance.
 func (sb *SailboatBuilder) Build() *Sailboat {
 	return sb.boat
 }
